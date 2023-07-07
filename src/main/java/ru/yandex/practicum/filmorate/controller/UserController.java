c06e4a47324930d2a37cd964151e7d114be1003b
d-dagoth <d-dagoth@ya.ru>
2023-07-08 00:57:20 +0300

    create add friends likes

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,52 +1,70 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import javax.validation.Valid;
-import javax.validation.ValidationException;
-import java.util.ArrayList;
-import java.util.HashMap;
+import javax.validation.constraints.NotNull;
 import java.util.List;
-import java.util.Map;
 
-@Slf4j
 @RestController
+@RequiredArgsConstructor
+@Validated
 public class UserController {
-    private static final Map<Long, User> users = new HashMap<>();
 
-    private long generatorId = 0;
-
-    private long generateId() {
-        return ++generatorId;
-    }
+    private final UserService userService;
 
     @GetMapping("/users")
     public List<User> getUsers() {
-        log.debug("getUsers");
-        return new ArrayList<>(users.values());
+        return userService.getUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable @NotNull Long id) {
+        return userService.getUser(id);
+    }
+
+    @GetMapping("/users/{userId}/friends")
+    public List<User> getFriendsUser(@PathVariable @NotNull Long userId) {
+        return userService.getFriendsUser(userId);
+    }
+
+    @GetMapping("/users/{userId}/friends/common/{friendId}")
+    public List<User> mutualFriends(@PathVariable @NotNull Long userId,
+                                    @PathVariable @NotNull Long friendId) {
+        return userService.mutualFriends(userId, friendId);
     }
 
     @PostMapping("/users")
     public User postUsers(@RequestBody @Valid User user) {
-        log.info("postUser-start: {}", user);
-        if (user.getName() == null || user.getName().isEmpty()) {
-            user.setName(user.getLogin());
-        }
-        user.setId(generateId());
-        users.put(user.getId(), user);
-        return user;
+        return userService.postUsers(user);
     }
 
     @PutMapping("/users")
     public User putUsers(@RequestBody @Valid User user) {
-        log.info("postUser-start: {}", user);
-        if (users.containsKey(user.getId())) {
-            users.put(user.getId(), user);
-            return user;
-        } else {
-            throw new ValidationException("UserId: " + user.getId());
-        }
+        return userService.putUsers(user);
+    }
+
+    @DeleteMapping("/users")
+    public void delUsers() {
+        userService.delUsers();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public User delUser(@PathVariable @NotNull Long id) {
+        return userService.delUser(id);
+    }
+
+    @PutMapping("/users/{userId}/friends/{friendId}")
+    public void addFriend(@PathVariable @NotNull Long userId, @PathVariable @NotNull Long friendId) {
+        userService.addFriend(userId, friendId);
+    }
+
+    @DeleteMapping("/users/{userId}/friends/{friendId}")
+    public void deleteFriend(@PathVariable @NotNull Long userId, @PathVariable @NotNull Long friendId) {
+        userService.deleteFriend(userId, friendId);
     }
 }
